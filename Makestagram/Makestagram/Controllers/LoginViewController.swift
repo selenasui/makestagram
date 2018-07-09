89ddca8f79a4a2312a2ed49999135546ca6ed6ee
Selena Sui <selena_sui@yahoo.com>
2018-07-09 15:38:30 -0700

    Refactored users

--- Makestagram/Makestagram/Controllers/LoginViewController.swift
+++ Makestagram/Makestagram/Controllers/LoginViewController.swift
@@ -63,8 +63,8 @@ extension LoginViewController: FUIAuthDelegate {
         let userRef = Database.database().reference().child("users").child(user.uid)
         
         userRef.observeSingleEvent(of: .value, with: { (snapshot) in
-            if let userDict = snapshot.value as? [String : Any] {
-                print("User already exists \(userDict.debugDescription).")
+            if let user = User(snapshot: snapshot) {
+                print("Welcome back, \(user.username).")
             } else {
                 print("New user!")
             }
