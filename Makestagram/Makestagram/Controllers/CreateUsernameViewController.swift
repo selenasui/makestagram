231741b55b0ca6366e22ea7b58c2fc31fea94361
Selena Sui <selena_sui@yahoo.com>
2018-07-09 16:48:14 -0700

    Refactored new users to database

--- Makestagram/Makestagram/Controllers/CreateUsernameViewController.swift
+++ Makestagram/Makestagram/Controllers/CreateUsernameViewController.swift
@@ -32,19 +32,10 @@ class CreateUsernameViewController: UIViewController {
             let username = usernameTextField.text,
             !username.isEmpty else { return }
         
-        let userAttrs = ["username": username]
-        
-        let ref = Database.database().reference().child("users").child(firUser.uid)
-        
-        ref.setValue(userAttrs) { (error, ref) in
-            if let error = error {
-                assertionFailure(error.localizedDescription)
-                return
-            }
+        UserService.create(firUser, username: username) { (user) in
+            guard let user = user else { return }
             
-            ref.observeSingleEvent(of: .value, with: { (snapshot) in
-                let user = User(snapshot: snapshot)
-            })
+            print("Created new user: \(user.username)")
         }
     }
     
