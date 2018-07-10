7495b90ae5084e413c1e9a958d0309959b8267ad
Selena Sui <selena_sui@yahoo.com>
2018-07-10 10:34:55 -0700

    Set up UITabBarController

---
+++ Makestagram/Makestagram/Controllers/FindFriendsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FindFriendsViewController.swift
+//  Makestagram
+//
+//  Created by Binjia Chen on 7/10/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import UIKit
+
+class FindFriendsViewController: UIViewController {
+    
+}
